ea7ffd6d6f31bb29505600ba86a30423ca4c01f4
km0401 <87297544+km0401@users.noreply.github.com>
2022-09-17 23:51:26 +0530

    User_Profile

---
+++ app/src/main/java/com/example/android_dev_carcool/User_Profile.java
@@ -0,0 +1,16 @@
+package com.example.android_dev_carcool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+
+    }
+}
