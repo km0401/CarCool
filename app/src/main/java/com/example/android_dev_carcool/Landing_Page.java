f4627b11a14a6e891ba85fb3891103660579e517
km0401 <87297544+km0401@users.noreply.github.com>
2022-09-18 16:30:12 +0530

    Landing_Page

---
+++ app/src/main/java/com/example/android_dev_carcool/Landing_Page.java
@@ -0,0 +1,14 @@
+package com.example.android_dev_carcool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Landing_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
