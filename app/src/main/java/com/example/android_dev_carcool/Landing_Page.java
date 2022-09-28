a4b46421f1199dcf3c9eb2e3677be22cafbd573b
km0401 <87297544+km0401@users.noreply.github.com>
2022-09-28 22:35:20 +0530

    Java Code Updated

--- app/src/main/java/com/example/android_dev_carcool/Landing_Page.java
+++ app/src/main/java/com/example/android_dev_carcool/Landing_Page.java
@@ -3,12 +3,34 @@ package com.example.android_dev_carcool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Landing_Page extends AppCompatActivity {
+
+    EditText leavingFrom;
+    EditText goingTo;
+    EditText date;
+    EditText noOfPassengers;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        leavingFrom = findViewById(R.id.leaving_from_text);
+        goingTo = findViewById(R.id.going_to_text);
+        date = findViewById(R.id.date_input);
+        noOfPassengers = findViewById(R.id.passenger_input);
+        searchButton = findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android_dev_carcool/User_Profile.java
+++ app/src/main/java/com/example/android_dev_carcool/User_Profile.java
@@ -2,15 +2,60 @@ package com.example.android_dev_carcool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class User_Profile extends AppCompatActivity {
+
+    TextView addBio;
+    TextView miniBio;
+    TextView add_preferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        addBio = findViewById(R.id.add_bio);
+        miniBio = findViewById(R.id.mini_bio);
 
+        addBio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+    }
+
+    void showCustomDialog() {
+        final Dialog dialog = new Dialog(this);
+        //We have added a title in the custom layout. So let's disable the default title.
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //The user will be able to cancel the dialog bu clicking anywhere outside the dialog.
+        dialog.setCancelable(true);
+        //Mention the name of the layout of your custom dialog.
+        dialog.setContentView(R.layout.custom_dialog);
+
+        //Initializing the views of the dialog.
+        final EditText nameEt = dialog.findViewById(R.id.name_et);
+        Button submitButton = dialog.findViewById(R.id.submit_button);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEt.getText().toString();
+                dialog.dismiss();
+                miniBio.setText(name);
+                addBio.setVisibility(View.GONE);
+            }
+        });
+        dialog.show();
     }
 }
