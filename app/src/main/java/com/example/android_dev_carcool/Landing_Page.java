5df5de21866d604b99f09e1ba0e25036a65dac1c
aditigoel <aditi.goel.20cse@bmu.edu.in>
2022-11-25 12:00:55 +0530

    nav bar implementation

--- app/src/main/java/com/example/android_dev_carcool/Landing_Page.java
+++ app/src/main/java/com/example/android_dev_carcool/Landing_Page.java
@@ -1,13 +1,22 @@
 package com.example.android_dev_carcool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Landing_Page extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
 
     EditText leavingFrom;
     EditText goingTo;
@@ -19,6 +28,40 @@ public class Landing_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        navigationView= findViewById(R.id.bottom_nav_menu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container,new SearchFragment()).commit();
+        navigationView.setSelectedItemId(R.id.search_navbar);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch(item.getItemId()){
+
+                    case R.id.search_navbar:
+                        fragment=new SearchFragment();
+                        break;
+
+                    case R.id.rides_navbar:
+                        fragment=new RidesFragment();
+                        break;
+                    case R.id.publish_navbar:
+                        fragment=new PublishFragment();
+                        break;
+                    case R.id.profile_navbar:
+                        fragment=new ProfileFragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container,fragment).commit();
+
+                return true;
+            }
+        });
+
         leavingFrom = findViewById(R.id.leaving_from_text);
         goingTo = findViewById(R.id.going_to_text);
         date = findViewById(R.id.date_input);
